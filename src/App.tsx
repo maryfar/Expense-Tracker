bcbd0b7608414784d3755c4ce69d958a1584f012
maryam.f <maryam.f@smartech.ir>
2026-04-29 22:32:44 +0330

    chore: initialize new project with base structure

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className=" flex flex-col items-center justify-center gap-6 mt-20">
+      <h1 className="text-red-900 text-3xl  ">Expense Tracker</h1>
+    </div>
+  );
+}
